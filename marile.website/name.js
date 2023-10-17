05613c4cda987d6c6126ec375efa3ab7e1a986ee
chandrakala <chandhu6302886523@gmail.com>
2023-10-17 23:45:12 +0530

    done

---
+++ marile.website/name.js
@@ -0,0 +1,39 @@
+console.log("chandrakala");
+	let num=2004;
+	if(num%4==0){
+	console.log("leap year");
+	}else{
+	console.log("not leap year");
+	}
+                 
+                 
+                 
+                 
+                 
+         let alpha="chandrakala";
+         let factor=0;
+         	for(let i=0;i<alpha.length;i++)
+         	{
+         	if(alpha[i]=="a" || alpha[i]=="e" || alpha[i]=="i"|| alpha[i]=="o"|| alpha[i]=="u")  {
+         		factor++;
+         		}
+         		}
+         		console.log(factor);   
+         		
+         		
+         		
+         		 
+                 
+               let str="amma";
+               let count="";
+               		for(let i=0;i<str.length;i++)
+               		{
+               			if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u")
+               			{
+               			count+=str[i];
+               			}
+               			
+               		}
+               		console.log(count);
+               			
+
